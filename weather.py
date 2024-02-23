b2d6d723db41473329f67da6679ae2291abd2da0
Wakeru <103467521+Wakeru@users.noreply.github.com>
2024-02-23 13:41:51 -0500

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,6 +1,6 @@
 import subprocess
 import sys
-import tkinter as tk #this is a GUI kit
+import tkinter as tk #this is a GUI kit for window and widgets
 import time
 import requests
 
@@ -14,6 +14,7 @@ except ImportError:
     
 def getWeather(window):
     city = textfield.get()
+    #im using my own API and if it doesn't show, then the free trial ended... it is what it is!
     api = "https://api.openweathermap.org/data/2.5/weather?q="+ city +"&appid=880b6a7cd415b87ca46867f8df99c4a0"
     json_data = requests.get(api).json()
     condition = json_data['weather'][0]['main'] 
@@ -31,23 +32,34 @@ def getWeather(window):
     label1.config(text=final_info)
     label2.config(text=final_data)
 
-
-window = tk.Tk()
+# initiating the window
+window = tk.Tk() 
+#making the window size
 window.geometry("1920x1080")
 window.title("Natalies Weather App")
 
-f = ("poppins", 15, "bold") #asinging variable to a font to make it easier to just plug in
+#asinging variable to a font to make it easier to just plug in
+f = ("poppins", 15, "bold") 
 t = ("poppins", 35, "bold")
 
+#making the textfield widget with working typing feature
 textfield = tk.Entry(window, font = t)
+#Padding around the text field
 textfield.pack(pady = 20)
+#the program starts, and the user can start typing immediately without needing to click on the widget first, FOCUSSS
 textfield.focus()
+#Whenver you hit RETURN on your keyboard, it starts up the getWeather with the value in the textfield
 textfield.bind("<Return>", getWeather)
 
-label1 = tk.Label(window,font = t)
+
+#these are label widgets to display text, for the humidty and sunrise and such
+label1 = tk.Label(window,font = t) 
 label1.pack()
 
 label2 = tk.Label(window,font = f)
 label2.pack()
 
-window.mainloop()#this runs the window itself
+label3 = tk.Label(window,text="I don't know why am I here...")
+label3.pack(side="left")
+
+window.mainloop()#this runs the window itself or making it loop itself forever until you exits
